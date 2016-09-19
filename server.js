07f5720f0319d182ddd7f704087dea53e31827e3
Junyong <zengjunyong@gmail.com>
2016-09-19 09:53:35 +0800

    use debug instead of console.log, run it via command: DEBUG=* node server.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
+var debug = require('debug')('server');
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+    debug('Example app listening on port 3000!');
 });
