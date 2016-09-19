6e0d828bd44f1673cba469110f137b44f99157b5
Junyong <zengjunyong@gmail.com>
2016-09-19 12:41:05 +0800

    mongoose.set('debug',true); read from mean.io package

--- mongoose.js
+++ mongoose.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/mongoose');
 
+mongoose.set('debug',true);
+
 var Cat = mongoose.model('Cat', { name: String });
 
 var kitty = new Cat({ name: 'Zildjian' });
