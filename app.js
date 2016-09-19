ebfe1cf93e4289bcca4c46f5f2293b8c019088f6
Junyong <zengjunyong@gmail.com>
2016-09-19 13:31:33 +0800

    helmet: Production Best Practices: Security http://expressjs.com/en/advanced/best-practice-security.html

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var compression = require('compression')
+var helmet = require('helmet');
 var debug = require('debug')('js-full-stack:app');
 
 var routes = require('./routes/index');
@@ -19,6 +20,7 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(helmet());
 app.use(compression({level:-1}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
