1fbcee5ae2a11e253fc9c3b6e74aa2caae169d56
Junyong <zengjunyong@gmail.com>
2016-09-19 11:49:59 +0800

    Built-in middleware http://expressjs.com/en/guide/using-middleware.html

--- app.js
+++ app.js
@@ -21,7 +21,17 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'),{
+  dotfiles: 'ignore',
+  etag: false,
+  extensions: ['htm', 'html'],
+  index: false,
+  maxAge: '1d',
+  redirect: false,
+  setHeaders: function (res, path, stat) {
+    res.set('x-timestamp', Date.now());
+  }
+}));
 
 app.use(function(req, res,next) {
   debug('Cookies: ', req.cookies);
