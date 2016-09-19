7ee5fb5550a7a3e38828f1f93c9a9cc8f749cc61
Junyong <zengjunyong@gmail.com>
2016-09-19 13:13:51 +0800

    moved the code out of callback, it worked well as well

--- mongoose/02.js
+++ mongoose/02.js
@@ -8,32 +8,33 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
     // we're connected!
     console.log("we're connected!");
+});
 
-    var kittySchema = mongoose.Schema({
-        name: String
-    });
-    // NOTE: methods must be added to the schema before compiling it with mongoose.model()
-    kittySchema.methods.speak = function () {
-        var greeting = this.name
-            ? "Meow name is " + this.name
-            : "I don't have a name";
-        console.log(greeting);
-    }
 
-    var Kitten = mongoose.model('Kitten', kittySchema);
+var kittySchema = mongoose.Schema({
+    name: String
+});
+// NOTE: methods must be added to the schema before compiling it with mongoose.model()
+kittySchema.methods.speak = function () {
+    var greeting = this.name
+        ? "Meow name is " + this.name
+        : "I don't have a name";
+    console.log(greeting);
+}
 
-    var silence = new Kitten({name: 'Silence'});
-    console.log(silence.name); // 'Silence'
+var Kitten = mongoose.model('Kitten', kittySchema);
 
-    var fluffy = new Kitten({name: 'fluffy'});
-    fluffy.speak(); // "Meow name is fluffy"
+var silence = new Kitten({name: 'Silence'});
+console.log(silence.name); // 'Silence'
 
-    fluffy.save(function (err, fluffy) {
-        if (err) return console.error(err);
-        fluffy.speak();
-    });
+var fluffy = new Kitten({name: 'fluffy'});
+fluffy.speak(); // "Meow name is fluffy"
 
-    Kitten.find({name: /^fluff/}, function (err, kettens) {
-        console.log(kettens); 
-    });
+fluffy.save(function (err, fluffy) {
+    if (err) return console.error(err);
+    fluffy.speak();
+});
+
+Kitten.find({name: /^fluff/}, function (err, kettens) {
+    console.log(kettens);
 });
